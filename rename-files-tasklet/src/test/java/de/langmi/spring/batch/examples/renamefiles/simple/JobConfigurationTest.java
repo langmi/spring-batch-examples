48056828853fb4989f61294fac6734f1e870b1bc
langmi <daywalker2000@gmx.net>
2011-07-21 10:22:29 +0200

    preparations for the generic example

--- rename-files-tasklet/src/test/java/de/langmi/spring/batch/examples/renamefiles/simple/JobConfigurationTest.java
+++ rename-files-tasklet/src/test/java/de/langmi/spring/batch/examples/renamefiles/simple/JobConfigurationTest.java
@@ -35,7 +35,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @author Michael R. Lange <michael.r.lange@langmi.de> 
  */
 @ContextConfiguration(locations = {
-    "classpath*:spring/batch/job/renamefiles/*.xml",
+    "classpath*:spring/batch/job/renamefiles/simple/*.xml",
     "classpath*:spring/batch/setup/**/*.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class JobConfigurationTest {
