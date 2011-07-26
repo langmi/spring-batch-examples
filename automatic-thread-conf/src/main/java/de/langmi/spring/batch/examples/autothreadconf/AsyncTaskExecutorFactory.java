5331fdd955dedde5a7111852fb1a230d67a1abd3
langmi <daywalker2000@gmx.net>
2011-07-26 09:07:17 +0200

    fixed github issue #3 'example: automatic thread configuration'

---
+++ automatic-thread-conf/src/main/java/de/langmi/spring/batch/examples/autothreadconf/AsyncTaskExecutorFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 Michael R. Lange <michael.r.lange@langmi.de>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.langmi.spring.batch.examples.autothreadconf;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+/**
+ * Factory creates taskexecutor and sets it's concurrency limit to
+ * number of available cpu.
+ *
+ * @author Michael R. Lange <michael.r.lange@langmi.de>
+ */
+public class AsyncTaskExecutorFactory {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AsyncTaskExecutorFactory.class);
+
+    public static SimpleAsyncTaskExecutor createInstance() {
+        SimpleAsyncTaskExecutor instance = new SimpleAsyncTaskExecutor();
+
+        // set concurrencyLimit according to available processors
+        // real simple 1:1 relation
+        Runtime runtime = Runtime.getRuntime();
+        int nrCpu = runtime.availableProcessors();
+        instance.setConcurrencyLimit(nrCpu);
+
+
+        LOG.info("TaskExecutor ConcurrencyLimit:" + String.valueOf(nrCpu));
+
+        return instance;
+    }
+}
