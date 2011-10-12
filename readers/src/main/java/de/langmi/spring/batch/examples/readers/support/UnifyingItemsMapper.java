a756e2922909d49a9794178e1b88212fdd152e74
langmi <daywalker2000@gmx.net>
2011-10-12 19:14:21 +0200

    refactoring for CompositeItemStreamReader, can be used as util

--- readers/src/main/java/de/langmi/spring/batch/examples/readers/support/UnifyingItemsMapper.java
+++ readers/src/main/java/de/langmi/spring/batch/examples/readers/support/UnifyingItemsMapper.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.langmi.spring.batch.examples.readers.file.composite;
+package de.langmi.spring.batch.examples.readers.support;
 
 import java.util.List;
 
